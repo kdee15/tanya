ddf7dc2df52de5411042f5546be8454815631a88
kirk <kdee15@gmail.com>
2019-02-21 07:02:52 +0200

    build

---
+++ section-about.php
@@ -0,0 +1,9 @@
+<section class="o-block about h-100" style="background: #F2F2F2;">
+
+  <div class="container">
+    <div class="row">
+      <h1>ABOUT ME</h1>
+    </div>
+  </div>
+
+</section>
---
+++ section-front.php
@@ -0,0 +1,7 @@
+<section class="o-header o-home-header h-100">
+
+  <!-- C.1. PAGE HEADER --------------------------------- -->
+  <h1>Tanya Daniels</h1>
+  <!-- C.1. END ----------------------------------------- -->
+
+</section>
